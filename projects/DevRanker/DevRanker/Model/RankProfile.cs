cfba3caf698ad17e31442cfa2360c09caaffd52b
Mikkel Theut <mikkeltheut@gmail.com>
2024-05-16 12:05:45 +0200

    chore: VeryOnliner.

--- projects/DevRanker/DevRanker/Model/RankProfile.cs
+++ projects/DevRanker/DevRanker/Model/RankProfile.cs
@@ -3,24 +3,7 @@
     public class RankProfile
     {
         private double _rank;
-        public double Rank
-        {
-            get { return _rank; } set
-            {
-                if (value < 0.0)
-                {
-                    _rank = 0.0;
-                }
-                else if (value > 10.0)
-                {
-                    _rank = 10.0;
-                }
-                else
-                {
-                    _rank = value;
-                }
-            }
-        }
+        public double Rank {get { return _rank; } set{if (value < 0.0){_rank = 0.0;}else if (value > 10.0){_rank = 10.0;}else{_rank = value;}}}
         public Profile Profile {get; set;}
     }
 }

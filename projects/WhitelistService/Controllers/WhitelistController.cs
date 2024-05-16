e7758bee722a933282de98188f707fd9cf9bcf9f
patrickstolc <patrickstolc@gmail.com>
2024-05-16 11:18:45 +0200

    add message client

--- projects/WhitelistService/Controllers/WhitelistController.cs
+++ projects/WhitelistService/Controllers/WhitelistController.cs
@@ -1,3 +1,4 @@
+using Messages;
 using Microsoft.AspNetCore.Mvc;
 using WhitelistService.Core.Common;
 using WhitelistService.Core.Schemas;
@@ -11,6 +12,13 @@ public class WhitelistController : ControllerBase
     [HttpPost]
     public Task<Response<AddProfileResponseData>> AddProfileToWhitelist([FromBody]AddProfileRequestData request)
     {
+        // Create remove profile message
+        RemoveProfileMessage message = new()
+        {
+            UserId = request.UserId,
+            ProfileId = request.ProfileId
+        };
+        
         // Add profile to whitelist
         return Task.FromResult(Response<AddProfileResponseData>.Ok(new AddProfileResponseData()));
     }

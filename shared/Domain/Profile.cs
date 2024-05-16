a3328f89316e00152a9254368587a3e80d38d97d
Sergio M <sergiomorenomartinez544@gmail.com>
2024-05-16 11:14:59 +0200

    Created Domain + Profile Service

---
+++ shared/Domain/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain;
+
+public class Profile {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Stack TechStack { get; set; }
+    public bool RemoteWork { get; set; }
+    public int Age { get; set; }
+    public string? Location { get; set; }
+    public int LikeCount { get; set; }
+}

24f4e4e9b6dfdb9c71821969074c2db8837970f3
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-05-16 12:04:44 +0200

    Suggestion startup

---
+++ projects/Suggestion/Suggestion/Core/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Suggestion.Core.Entities;
+
+namespace Suggestion.Core.Repository
+{
+    public interface IRepository
+    {
+        public List<RankProfile> GetRankProfiles();
+
+        public Entiies.Suggestion CreateSuggestion();
+
+        public List<Entiies.Suggestion> GetSuggestions();
+
+
+    }
+}
---
+++ projects/Suggestion/Suggestion/Core/Repository/Repository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Suggestion.Core.Entities;
+
+namespace Suggestion.Core.Repository
+{
+    public class Repository: IRepository
+    {
+
+        private DbContextOptions<RepositoryDBContext> _options;
+
+        public Repository(RepositoryDBContext dbContext)
+        {
+            _options = new DbContextOptionsBuilder<RepositoryDBContext>().UseInMemoryDatabase("SuggestionDB").Options;
+        }
+
+        public Entiies.Suggestion CreateSuggestion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RankProfile> GetRankProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entiies.Suggestion> GetSuggestions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

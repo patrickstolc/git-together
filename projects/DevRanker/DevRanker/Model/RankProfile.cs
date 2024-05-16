d18e4443a1899f90d067dc65bba4ecc895d556b3
Mikkel Theut <mikkeltheut@gmail.com>
2024-05-16 12:14:20 +0200

    fix:Using Patricks Min Max

--- projects/DevRanker/DevRanker/Model/RankProfile.cs
+++ projects/DevRanker/DevRanker/Model/RankProfile.cs
@@ -2,8 +2,15 @@
 {
     public class RankProfile
     {
+
         private double _rank;
-        public double Rank {get { return _rank; } set{if (value < 0.0){_rank = 0.0;}else if (value > 10.0){_rank = 10.0;}else{_rank = value;}}}
+        public double Rank
+        {
+            get { return _rank; } set
+            {
+                _rank = Math.Min(Math.Max(value, 0), 10);
+            }
+        }
         public Profile Profile {get; set;}
     }
 }

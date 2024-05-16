7071d25c173156e8d34acb594fbbc47ddb08d7a8
Mikkel Theut <mikkeltheut@gmail.com>
2024-05-16 11:46:39 +0200

    chore: beginning, needs baseurl and requeststring

---
+++ projects/DevRanker/DevRanker/Model/RankProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace DevRanker.Model
+{
+    public class RankProfile
+    {
+        private double _rank;
+        public double Rank
+        {
+            get { return _rank; } set
+            {
+                if (value < 0.0)
+                {
+                    _rank = 0.0;
+                }
+                else if (value > 10.0)
+                {
+                    _rank = 10.0;
+                }
+                else
+                {
+                    _rank = value;
+                }
+            }
+        }
+        public Profile Profile {get; set;}
+    }
+}
